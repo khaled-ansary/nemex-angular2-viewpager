4efde91f0df64052625fa4ddb1a10bee384dab8c
hadar <hadar753@gmail.com>
2017-10-30 11:48:42 +0200

    remove browserAnimation

--- src/nemex-viewpager.module.ts
+++ src/nemex-viewpager.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ViewPagerComponent } from './viewpager.component';
 import { ViewPagerIndicatorComponent } from './viewpager.indicator.component';
 
 @NgModule({
-    imports: [CommonModule, BrowserAnimationsModule],
+    imports: [CommonModule],
     declarations: [ViewPagerComponent, ViewPagerIndicatorComponent],
     exports: [ViewPagerComponent, ViewPagerIndicatorComponent],
     providers: [],
